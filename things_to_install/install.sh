75c38c97249dd9f336f62ababd37cf84d3dd0c6a
Mark Koh <markkohdev@gmail.com>
2020-03-12 17:59:27 -0400

    Fix some install stuff

--- things_to_install/install.sh
+++ things_to_install/install.sh
@@ -1,7 +1,14 @@
-!#/bin/bash
+#!/bin/bash
+# Should be run from dotfiles repo root
 
-SCRIPT_DIR="$(dirname $(realpath $0))"
+cd "$(dirname "$0")/.."
+DOTFILES_ROOT=$(pwd)
+SCRIPT_DIR="$DOTFILES_ROOT/things_to_install"
 
+# Install oh-my-zsh
+sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+
+# Install brew dependencies
 if [ ! -z $(which brew) ]; then
   brew bundle --file $SCRIPT_DIR/Brewfile
 else
@@ -9,8 +16,11 @@ else
   echo "To install brew, see https://brew.sh/"
 fi
 
+# TODO: Add equivalent apt-get dependencies
+
+# Install pip dependencies
 if [ ! -z $(which pip) ]; then
   pip install -r $SCRIPT_DIR/requirements.txt
 else
-    echo "Pip installation not found.  Skipping pip installation."
+  echo "Pip installation not found.  Skipping pip installation."
 fi
